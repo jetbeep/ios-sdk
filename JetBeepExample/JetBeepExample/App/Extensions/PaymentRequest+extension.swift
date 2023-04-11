5a3f49fa2b5f7fe0a4df49f99362737b62091ec9
Max Tymchii <samback@ukr.net>
2023-04-11 16:14:54 +0100

    Add payment request extension.

---
+++ JetBeepExample/JetBeepExample/App/Extensions/PaymentRequest+extension.swift
@@ -0,0 +1,23 @@
+//
+//  PaymentRequest+extension.swift
+//  JetBeepExample
+//
+//  Created by Max Tymchii on 10.04.2023.
+//  Copyright © 2023 Max Tymchii. All rights reserved.
+//
+
+import Foundation
+import JetBeepFramework
+
+extension PaymentRequest {
+
+    var body: JSONDictionary {
+        ["vendor_transaction_id" : transactionId,
+         "vendor_amount" : amount,
+         "vendor_cashier_id" : cashierId ?? "0",
+         "shop_id" : shop.id,
+         "device_id" : deviceId,
+         "signature" : signature ?? ""]
+    }
+}
+
